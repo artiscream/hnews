cd96f2b27257b121541fe7e4dcbb01721e2f78df
artiscream <arti_scream@ukr.net>
2019-01-24 11:22:55 +0200

    some assoc mes and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_090611) do
+ActiveRecord::Schema.define(version: 2019_01_24_092023) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
